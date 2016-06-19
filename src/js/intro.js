9dcd0c1b143aada614cae466f7ac548b18cc9c7a
Etienne <e.e.f.prudhomme@gmail.com>
2016-06-18 21:29:07 -0400

    adds a temporary fix for Web Components version
    * src/js/intro.js (windowUdacityFEGradingEngine): Declares a components
      reference as document. It’s a temporary fix to allow the build to work
      when refactoring.

--- src/js/intro.js
+++ src/js/intro.js
@@ -9,5 +9,6 @@ Udacity’s library for immediate front-end feedback.
 ;window.UdacityFEGradingEngine ? window.UdacityFEGradingEngine = window.UdacityFEGradingEngine : window.UdacityFEGradingEngine = (function(window, undefined) {
   'use strict';
   var exports = {};
+  var components = document;
 
 // intro.js ends here
