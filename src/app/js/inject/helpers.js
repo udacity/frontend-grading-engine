05d02363ff92e6c373ad3b5fe85a608a3426b87a
Etienne <e.e.f.prudhomme@gmail.com>
2016-06-18 22:26:05 -0400

    moves helper functions outside of `inject.js`
    * src/app/js/inject/inject.js (injectIntoDocument): Moves this function
      to `helpers.js`.

---
+++ src/app/js/inject/helpers.js
@@ -0,0 +1,45 @@
+/**
+ * @fileoverview This file contains various functions that aren’t specific to the current extension.
+ */
+
+/**
+ * Adds elements to the main page.
+ * @param  {String} tag       Type of element
+ * @param  {Object} data      Key/value pairs you want to be assigned to as newTag[key] = value
+ * @param  {Object} location  Set to “head” if you want the element to end up there. Default is body
+ * @return {Promise}
+ */
+function injectIntoDocument(tag, data, location) {
+  location = location || 'body';
+  return new Promise(function(resolve, reject) {
+    var newTag = document.createElement(tag);
+    if (data) {
+      for (a in data) {
+        newTag[a] = data[a];
+      }
+    }
+
+    if (!newTag.id) {
+      newTag.id = 'ud-' + Math.floor(Math.random() * 100000000).toString();
+    }
+    // for later removal
+    injectedElementsOnPage.push(newTag.id);
+
+    newTag.onload = function(e) {
+      resolve(e);
+    };
+    newTag.onerror = function(e) {
+      reject(e);
+    };
+    if (tag === 'script' && !newTag.src && (newTag.text || newTag.innerHTML)) {
+      resolve();
+    }
+    if (location === 'head') {
+      document.head.appendChild(newTag);
+    } else {
+      document.body.appendChild(newTag);
+    };
+  });
+}
+
+// helpers.js ends here
