e11a898ee3bdabdc192cea517b3d652d9ca644e0
Etienne <e.e.f.prudhomme@gmail.com>
2016-06-22 23:58:58 -0400

    moves `chrome.storage` polyfill in `intro.js`
    * src/app/js/inject/StateManager.js (chrome.storage): moved in
      `intro.js` since it’s specific to Firefox.

--- src/app/js/inject/StateManager.js
+++ src/app/js/inject/StateManager.js
@@ -2,50 +2,6 @@
  * @fileoverview This file contains the StateManager Class.
  */
 
-chrome.storage = {
-  sync: {
-    /**
-     * Gets one or more items from storage.
-     * @param {string|string[]|object} [keys] - A single key to get, list of keys to get, or a dictionary specifying default values (see description of the object). An empty list or object will return an empty result object. Pass in null to get the entire contents of storage
-     * @param {function} callback - Callback with storage items, or on failure (in which case runtime.lastError will be set).
-     * @returns {}
-     */
-    get: function(keys, callback) {
-      debugger;
-      console.log(callback);
-      console.log(callback.toString());
-      var message = {};
-      message.data = keys;
-      message.type = 'chrome.storage.local.get';
-
-      chrome.runtime.sendMessage(null, message, {}, localHandleGet);
-
-      function localHandleGet(response) {
-        debugger;
-        console.log('localHandleGet');
-        callback(response);
-      }
-    },
-    set: function(object, callback) {
-      debugger;
-      console.log("chrome.storage.sync.set object = ", object);
-      var message = {};
-
-      message.type = 'chrome.storage.local.set';
-      message.data = object;
-      chrome.runtime.sendMessage(null, message, {}, localHandleSet);
-      function localHandleSet(response) {
-        debugger;
-        console.log('localHandleSet');
-        if(response.status) {
-          throw new Error('Error: ' + response.message);
-        }
-        callback();
-      }
-    }
-  }
-};
-
 /**
  * State of the current Document.
  * @returns {Promise}
