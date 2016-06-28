48624301f83b4d8d0e1ec2d72fe417fa371f3063
Etienne <e.e.f.prudhomme@gmail.com>
2016-06-28 02:28:22 -0400

    adds build support for chromium and firefox

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,207 @@
 var gulp = require('gulp');
+var gulpsync = require('gulp-sync')(gulp);
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
 var debug = require('gulp-debug');
 var batch = require('gulp-batch');
 var uglify = require('gulp-uglify');
+var mv = require('mv');
 
-var build = 'build/';
+var currentBrowser;
 
-var jsFiles = [
-  'src/js/intro.js',
-  'src/js/helpers.js',
-  'src/js/Queue.js',
-  'src/js/Target.js',
-  'src/js/GradeBook.js',
-  'src/js/TACollectors.js',
-  'src/js/TAReporters.js',
-  'src/js/ActiveTest.js',
-  'src/js/Suite.js',
-  'src/js/registrar.js',
-  'src/js/outro.js'
-];
+var build = './build/%target%/ext/';
 
-var injectJsFiles = [
-  'firefox/inject/intro.js',
-  'src/app/js/inject/helpers.js',
-  'src/app/js/inject/StateManager.js',
-  'src/app/js/inject/inject.js',
-  'firefox/inject/outro.js'
-];
+var log = function(message) {
+  console.log('\x1b[37;46m####       ' + message + '\x1b[0;m');
+};
 
-var components = [
-  'src/app/test_widget/js/components.js',
-  'src/app/test_widget/js/test_suite.js',
-  'src/app/test_widget/js/test_results.js',
-  'src/app/test_widget/js/active_test.js',
-  'src/app/test_widget/js/test_widget.js'
-];
+var jsFiles = {
+  gradingEngine: {
+    src: [
+      'src/js/intro.js',
+      'src/js/helpers.js',
+      'src/js/Queue.js',
+      'src/js/Target.js',
+      'src/js/GradeBook.js',
+      'src/js/TACollectors.js',
+      'src/js/TAReporters.js',
+      'src/js/ActiveTest.js',
+      'src/js/Suite.js',
+      'src/js/registrar.js',
+      'src/js/outro.js'
+    ],
+    concat: 'GE.js',
+    dest: build + 'app/js/libs/'
+  },
+  libraries: {
+    src: 'src/app/js/libs/*.js',
+    dest: build + 'app/js/libs/'
+  },
+  background: {
+    src: '%target%/background.js',
+    dest: build + 'app/js/'
+  },
+  inject: {
+    src: [
+      '%target%/inject/intro.js',
+      'src/app/js/inject/helpers.js',
+      'src/app/js/inject/StateManager.js',
+      'src/app/js/inject/inject.js',
+      '%target%/inject/outro.js'
+    ],
+    concat: 'inject.js',
+    dest: build + 'app/js/'
+  },
+  components: {
+    src: [
+      'src/app/test_widget/js/components.js',
+      'src/app/test_widget/js/test_suite.js',
+      'src/app/test_widget/js/test_results.js',
+      'src/app/test_widget/js/active_test.js',
+      'src/app/test_widget/js/test_widget.js'
+    ],
+    concat: 'components.js',
+    dest: build + 'app/templates/'
+  }
+};
 
-var ui_v2 = [
-  'src/app/test_widget/test_widget.html',
-  'src/app/test_widget/test_widget.css'
-];
+var gradingEngine = jsFiles.gradingEngine;
+var libraries = jsFiles.libraries;
+var background = jsFiles.background;
+var inject = jsFiles.inject;
+var components = jsFiles.components;
 
-var iconFiles = 'src/icons/*.png';
+var browserPageFiles = {
+  pageAction: {
+    src: 'src/app/browser_action/browser_action.*',
+    dest: build + 'app/browser_action/'
+  },
+  pageOptions: {
+    src: ['src/app/options/index.html', 'src/app/options/options.js'],
+    dest: build + 'app/options/'
+  }
+};
+var pageAction = browserPageFiles.pageAction;
+var pageOptions = browserPageFiles.pageOptions;
 
-var allFiles = jsFiles.concat(injectJsFiles, components, ui_v2);
+var iconFiles = {
+  src: 'src/icons/*.png',
+  dest: build + 'icons/'
+};
 
-// Native components (see components.js)
-gulp.task('components', function() {
-  return gulp.src(components)
-    .pipe(concat('components.js'))
-    .pipe(gulp.dest(build + 'ext/app/templates/'))
-    .pipe(debug({title: 'built components: '}));
-});
+// Files to watch
+var allFiles = gradingEngine.src.concat(components.src, inject.src);
 
-// This is the iFrame document
-gulp.task('ui_v2', function() {
-  return gulp.src(ui_v2)
-  .pipe(gulp.dest(build + 'ext/app/templates/'))
-  .pipe(debug({title: 'built user interface v2: '}));
-});
-
-gulp.task('icons', function() {
-  return gulp.src(iconFiles)
-    .pipe(gulp.dest(build + 'ext/icons/'))
-    .pipe(debug({title: 'copied icons:'}));
-});
-
-// Browser independent procedures
+// "GE" = Build the GradingEngine library.
 gulp.task('GE', function() {
-  return gulp.src(jsFiles)
-    .pipe(concat('GE.js'))
-    .pipe(gulp.dest(build + 'ext/app/js/libs/'))
+  return gulp.src(gradingEngine.src)
+    .pipe(concat(gradingEngine.concat))
+    .pipe(gulp.dest(gradingEngine.dest))
     .pipe(debug({title: 'built dev grading engine:'}));
 });
 
+// "libraries" = Copy libraries of the application.
+gulp.task('libraries', function() {
+  return gulp.src(libraries.src)
+    .pipe(gulp.dest(libraries.dest))
+    .pipe(debug({title: 'copied grading engine libraries:'}));
+});
+
+// "components" = Generate the native components. There were
+// previously Web Components.
+gulp.task('components', function() {
+  return gulp.src(components.src)
+    .pipe(concat(components.concat))
+    .pipe(gulp.dest(components.dest))
+    .pipe(debug({title: 'built components: '}));
+});
+
+// "inject" = Generate the inject script for the current browser.
 gulp.task('inject', function() {
-  return gulp.src(injectJsFiles)
-    .pipe(concat('inject.js'))
-    .pipe(gulp.dest(build + 'ext/app/js/'))
-    .pipe(debug({title: 'build inject.js:'}));
+  var files = inject.src.map(function(x) {
+    return x.replace('%target%', currentBrowser);
+  });
+  return gulp.src(files)
+    .pipe(concat(inject.concat))
+    .pipe(gulp.dest(inject.dest))
+    .pipe(debug({title: 'built inject.js:'}));
 });
 
-// Temporary solution. App should be refactored with ui.
-gulp.task('app', function() {
-  return gulp.src('src/app/**/*')
-    .pipe(gulp.dest(build + 'ext/app/'))
-    .pipe(debug({title: 'copied app files:'}));
+// "pageAction" = Copy the `browser_action` page.
+gulp.task('pageAction', function() {
+  return gulp.src(pageAction.src)
+    .pipe(gulp.dest(pageAction.dest))
+    .pipe(debug({title: 'copied action page:'}));
 });
 
-gulp.task('chromium', ['app'], function() {
-  return gulp.src('chromium/manifest.json')
-    .pipe(gulp.dest(build + 'ext/'))
-    .pipe(debug({title: 'copied Chromium’s manifest:'}));
+// "pageOptions" = Copy the options page.
+gulp.task('pageOptions', function() {
+  return gulp.src(pageOptions.src)
+    .pipe(gulp.dest(pageOptions.dest))
+    .pipe(debug({title: 'copied options page:'}));
 });
 
-gulp.task('firefox', ['app'], function() {
-  return gulp.src('firefox/manifest.json')
-    .pipe(gulp.dest(build + 'ext/'))
-    .pipe(debug({title: 'copied Firefox’s manifest:'}));
+// "icons" = Copy the icons.
+gulp.task('icons', function() {
+  return gulp.src(iconFiles.src)
+    .pipe(gulp.dest(iconFiles.dest))
+    .pipe(debug({title: 'copied icons:'}));
 });
 
-gulp.task('default', ['firefox', 'inject', 'ui_v2', 'components', 'icons', 'GE']);
+// "app" = Executes tasks for the app (view).
+gulp.task('app', ['components', 'inject', 'pageAction', 'pageOptions', 'icons']);
+
+// "extension" = Executes tasks that are mostly not browser specific.
+gulp.task('extension', ['app', 'GE', 'libraries']);
+
+// "background-script" = Copy the background script for the
+// `currentBrowser` (if any).
+gulp.task('background-script', function() {
+  return gulp.src(background.src.replace('%target%', currentBrowser))
+    .pipe(gulp.dest(background.dest))
+    .pipe(debug({title: 'copied ' + currentBrowser + '’s background script:'}));
+});
+
+// "manifest" = Copy the manifest for the current browser
+gulp.task('manifest', function() {
+  return gulp.src(currentBrowser + '/manifest.json')
+    .pipe(gulp.dest(build))
+    .pipe(debug({title: 'copied ' + currentBrowser +'’s manifest:'}));
+});
+
+// "_chromium" = Sets currentBrowser to chromium.
+gulp.task('_chromium', function() {
+  currentBrowser = 'chromium';
+  return log('Moved ' + currentBrowser + ' files to: ' + currentBrowser);
+});
+
+// "_firefox" = Sets currentBrowser to firefox
+gulp.task('_firefox', function() {
+  currentBrowser = 'firefox';
+  return log('Moved ' + currentBrowser + ' files to: ' + currentBrowser);
+});
+
+// "chromium" = First run dependencies to build the extension and then
+// move those files to `build/chromium` instead of `build/%target%/`.
+gulp.task('chromium', gulpsync.sync(['_chromium', ['manifest', 'extension']]), function() {
+  var browserBuild = build.replace('%target%/ext/', currentBrowser + '/');
+  mv(build.replace('ext/', ''), browserBuild, {mkdirp: true}, function(err) {
+    console.log(err);
+  });
+  return log('Moved ' + currentBrowser + ' files to: ' + browserBuild);
+});
+
+// "firefox" = First run dependencies to build the extension and then
+// move those files to `build/firefox` instead of `build/%target%/`.
+gulp.task('firefox', gulpsync.sync(['_firefox', ['manifest', 'background-script', 'extension']]), function() {
+  var browserBuild = build.replace('%target%/ext/', 'firefox/');
+  mv(build.replace('ext/', ''), browserBuild, {mkdirp: true}, function(err) {
+    console.log(err);
+  });
+  return log('Firefox files to: ' + browserBuild);
+});
+
+gulp.task('default', gulpsync.sync(['firefox', 'chromium']));
 
 gulp.task('watch', function() {
   gulp.start('default');
