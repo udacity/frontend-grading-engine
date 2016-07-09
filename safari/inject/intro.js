5c01adec48754e1958b7006f630e5c5c4bc737c7
Etienne <e.e.f.prudhomme@gmail.com>
2016-07-08 21:06:33 -0400

    Add `inject.js` "advice"
    * safari/inject/intro.js: Adds initial `inject.js` specific tweaks for
      Safari.
    * safari/inject/outro.js: Adds outro

---
+++ safari/inject/intro.js
@@ -0,0 +1,13 @@
+/**
+ * @fileOverview This file contains the opening statements of `inject.js` for Safari.
+ * @name intro.js<safari>
+ * @author Etienne Prud’homme
+ * @license MIT
+ */
+
+/* jshint ignore:start */
+// Injected scripts in Safari get also injected in iFrames
+if (window.top === window) {
+  /* jshint ignore: */
+
+  // intro.js<safari> ends here
---
+++ safari/inject/outro.js
@@ -0,0 +1,13 @@
+/**
+ * @fileOverview This file contains the closing statements of `inject.js`.
+ * @name outro.js<Safari>
+ * @author Etienne Prud’homme
+ * @license MIT
+ */
+
+/* jshint ignore:start */
+}
+/* jshint ignore:end */
+
+// outro.js<firefox> ends here
+// inject.js<firefox> ends here
