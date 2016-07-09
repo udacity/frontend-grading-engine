0e5794f4fe49cb3127ef92f6d3c2317ba834d273
Etienne <e.e.f.prudhomme@gmail.com>
2016-07-08 20:00:29 -0400

    adds backgorund page documentation

--- safari/background.js
+++ safari/background.js
@@ -1,10 +1,18 @@
+/*global safari */
+
 /**
  * @fileOverview This file adds Safari support for those APIs:
- *   - {@link chrome.runtime}
  * TODO
  * @name background.js<safari>
  * @author Etienne Prud’homme
  * @license MIT
+ * Note:
+ * Injected Scripts don’t have access to the `chrome.*` API with the exception of:
+ * * `extension` (`getURL`, `inIncognitoContext`, `lastError`, `onRequest`, `sendRequest`)
+ * * `i18n`
+ * * `runtime` (`connect`, `getManifest`, `getURL`, `id`, `onConnect`, `onMessage`, `sendMessage`)
+ * * `storage`
+ * This is wĥy this background script is created.
  */
 
 // background.js<safari>
