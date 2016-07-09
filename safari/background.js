e8033dc113df6ffa1faaae85fb3cd59d702e0763
Etienne <e.e.f.prudhomme@gmail.com>
2016-07-08 20:24:39 -0400

    adds the Safari wrapper documentation

--- safari/background.js
+++ safari/background.js
@@ -15,4 +15,41 @@
  * This is wĥy this background script is created.
  */
 
+/**
+ * Adaptee that translates chrome method behavior to safari.
+ * @namespace
+ * @property {error} wrapper.runtime.lastError - Set for the lifetime of a callback if an ansychronous extension api has resulted in an error. If no error has occured lastError will be undefined.
+ */
+var wrapper = {
+  storage: {
+    sync: {
+      /**
+       * Emulates the chrome storage behavior (getter) by using the {@link safari.extension.settings} mechanism.
+       * @param {string|string[]|object} keys - A single key to get, list of keys to get, or a dictionary specifying default values (see description of the object). An empty list or object will return an empty result object. Pass in null to get the entire contents of storage.
+       * @returns {object} Object with items in their key-value mappings.
+       * @throws {error} Error in the {@link keys} argument and sets {@link wrapper.runtime.lastError}.
+       */
+      /**
+       * Emulates the chrome storage behavior (setter) by using the {@link safari.extension.settings} mechanism.
+       * @param {} keys - An object which gives each key/value pair to update storage with. Any other key/value pairs in storage will not be affected.
+       * Primitive values such as numbers will serialize as expected. Values with a typeof `object` and `function` will typically serialize to `{}`, with the exception of `Array` (serializes as expected), Date, and Regex (serialize using their `String` representation).
+       * @returns {int} 0 on success and -1 on error.
+       * @throws {error} Error in the {@link keys} argument and sets {@link wrapper.runtime.lastError}.
+       */
+    }
+  },
+  runtime: {
+    lastError: undefined
+  },
+  tabs: {
+    /**
+     * Gets all tabs that have the specified properties, or all tabs if no properties are specified.
+     * @param {object} queryInfo
+     * @param {bool} [queryInfo.active] - TODO Whether the tabs are active in their windows. (Does not necessarily mean the window is focused.)
+     * @param {bool} [queryInfo.currentWindow] - TODO Whether the tabs are in the /current window/.
+     * @param {string} tabId - The tab to return
+     */
+  }
+};
+
 // background.js<safari>
