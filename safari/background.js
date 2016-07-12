094634fa685078ce457ff1b60a0312168da052f3
Etienne <e.e.f.prudhomme@gmail.com>
2016-07-12 19:48:36 -0400

    refactors duplicate procdedures
    * safari/background.js (respondBack): Function that sends back the
      result of the request and also take cares of status codes.

--- safari/background.js
+++ safari/background.js
@@ -214,56 +214,44 @@ var wrapper = {
 
 // Listens to the client adapter
 safari.application.addEventListener('message', function(event) {
-  var status = -1,
-      response = '';
+  var status = -1;
 
   // Safari uses ev.name for the name of the event while using /message/ for communication between scripts.
   switch(event.name) {
   case 'wrapper.storage.sync.get':
     // Returns -1 on error otherwise the response
     status = wrapper.storage.sync.get(event.message.keys);
-
-    if(status === -1) {
-      response = {status: 'error', message: wrapper.runtime.lastError.message};
-    } else {
-      response = {status: 'ok', response: status};
-    }
-    event.target.page.dispatchMessage('chrome.storage.sync.get', response);
+    respondBack('chrome.storage.sync.get', status);
     break;
   case 'wrapper.storage.sync.set':
     // Returns -1 on error otherwise the response
     status = wrapper.storage.sync.set(event.message.keys);
-
-    if(status === -1) {
-      response = {status: 'error', response: wrapper.runtime.lastError.message};
-    } else {
-      response = {status: 'ok', response: status};
-    }
-    event.target.page.dispatchMessage('chrome.storage.sync.set', status);
+    respondBack('chrome.storage.sync.set', status);
     break;
   case 'wrapper.runtime.sendMessage':
     // TODO
     // Returns -1 on error otherwise the response
     status = wrapper.runtime.sendMessage();
-    if(status === -1) {
-      response = {status: 'error', response:wrapper.runtime.lastError};
-      event.target.page.dispatchEvent('chrome.runtime.sendMessage', status);
-    }
+    respondBack('chrome.runtime.sendMessage', status);
     break;
   case 'wrapper.tabs.query':
     // Returns -1 on error otherwise the response
     status = wrapper.tabs.query(event.message.query);
 
     // Note: The docs don’t officially specify throwing lastError
+    respondBack('chrome.tabs.query', status);
+    break;
+  }
+
+  function respondBack(channel, status) {
+    var response;
     if(status === -1) {
       response = {name: 'error', response: wrapper.runtime.lastError.message};
     } else {
       response = {name: 'ok', response: status};
     }
-    event.target.page.dispatchEvent('chrome.tabs.query', response);
-    break;
+    event.target.page.dispatchEvent(channel, response);
   }
-
   // Since its lifetime is for a callback
   wrapper.runtime.lastError = undefined;
 }, false);
