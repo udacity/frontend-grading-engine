21bdfbf1aa4ef3bddae39d48959f357d6a5d1ef9
Etienne <e.e.f.prudhomme@gmail.com>
2016-07-23 20:30:37 -0400

    adds interactivity to the options page
    * src/app/options/options.js (_refreshDisplay): Adds a function to fill
      the table with the whitelist items.
    * (newEntry): Adds a function to add an entry to the whitelist table.
    * (newOriginEntry): Adds a function to add an entry inside the origin
      section of the whitelist table.

--- src/app/options/options.js
+++ src/app/options/options.js
@@ -4,14 +4,19 @@
  * @fileOverview This file contains the option page for adding/removing websites from the whitelist.
  * @name options.js<options>
  * @author Cameron Pittman
+ * @author Etienne Prud’homme
  * @license GPLv3
  */
 
 var wlSites = document.querySelector('#wl-sites');
 var removeSitesDatalist = document.querySelector('#remove-site');
-
 var siteToAdd = document.querySelector('input#add-site');
 var siteToRemove = document.querySelector('#site-to-remove');
+
+var originWhitelist = document.querySelector('#origin-whitelist');
+var localWhitelist = document.querySelector('#local-whitelist');
+
+var isChromium = window.navigator.vendor.toLocaleLowerCase().indexOf('google') !== -1;
 
 function StateManager() {
   this.whitelist = [];
@@ -19,7 +24,7 @@ function StateManager() {
 
 StateManager.prototype = {
   getWhitelist: function() {
-  	var self = this;
+    var self = this;
     return new Promise(function (resolve, reject) {
       chrome.storage.sync.get('whitelist', function (response) {
         self.whitelist = response.whitelist;
@@ -61,40 +66,75 @@ StateManager.prototype = {
 };
 
 function refreshDisplay() {
-	siteToAdd.value = '';
+  siteToAdd.value = '';
 
-	removeSitesDatalist.textContent = '';
-	siteToRemove.value = '';
+  removeSitesDatalist.textContent = '';
+  siteToRemove.value = '';
 
-	stateManager.whitelist.forEach(function (val) {
-	  var o = document.createElement('option');
-	  o.for = "remove-site";
-	  o.value = val;
-	  removeSitesDatalist.appendChild(o);
-	});
-	if (stateManager.whitelist.length > 0) {
-		wlSites.textContent = stateManager.whitelist.join(', ');
-	};
+  stateManager.whitelist.forEach(function (val) {
+    var o = document.createElement('option');
+    o.for = "remove-site";
+    o.value = val;
+    removeSitesDatalist.appendChild(o);
+  });
+  if (stateManager.whitelist.length > 0) {
+    wlSites.textContent = stateManager.whitelist.join(', ');
+  };
 };
+
+function _refreshDisplay() {
+  var whitelist = stateManager.whitelist;
+  var isEmpty = true, whitelistElem;
+
+  for(var i=0, len=whitelist.length; i<len; i++) {
+    if(whitelist[i]) {
+      newOriginEntry(whitelist[i]);
+      isEmpty = false;
+    }
+  }
+
+  if(!isEmpty) {
+    whitelistElem = document.getElementById('origin-whitelist');
+    whitelistElem.removeChild(whitelistElem.getElementsByClassName('whitelist-placeholder')[0]);
+  }
+}
+
+function newEntry(data, type) {
+  var template = document.getElementById('whitelist-entry-template');
+  var entry = template.cloneNode(true);
+  entry.removeAttribute('id');
+  entry.getElementsByClassName('entry')[0].textContent = data;
+  entry.getElementsByClassName('remove-entry')[0].addEventListener('click', function handler(event) {
+    event.preventDefault();
+    entry.parentElement.removeChild(entry);
+    window.dispatchEvent(new CustomEvent('remove', {detail: {type: type, data: data}}));
+  });
+  return entry;
+}
+
+function newOriginEntry(url) {
+  originWhitelist.appendChild(newEntry(url, 'origin'));
+}
 
 var stateManager = new StateManager();
 stateManager.getWhitelist()
-.then(refreshDisplay);
+  .then(refreshDisplay)
+  .then(_refreshDisplay);
 
 siteToAdd.onkeyup = function(e) {
-	if (e.keyCode === 13) {
-		var site = siteToAdd.value;
-		stateManager.addSiteToWhitelist(site)
-		.then(refreshDisplay);
-	}
+  if (e.keyCode === 13) {
+    var site = siteToAdd.value;
+    stateManager.addSiteToWhitelist(site)
+      .then(refreshDisplay);
+  }
 };
 
 siteToRemove.onkeyup = function(e) {
-	if (e.keyCode === 13) {
-		var site = siteToRemove.value;
-		stateManager.removeSiteFromWhitelist(site)
-		.then(refreshDisplay);
-	}
+  if (e.keyCode === 13) {
+    var site = siteToRemove.value;
+    stateManager.removeSiteFromWhitelist(site)
+      .then(refreshDisplay);
+  }
 };
 
 // options.js<options> ends here
