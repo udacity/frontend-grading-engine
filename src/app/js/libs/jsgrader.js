562ee9dfd09abecef39e90d264b32e9f9d58b561
Etienne <e.e.f.prudhomme@gmail.com>
2016-11-29 16:55:36 -0500

    Lints the jsgrader comments

--- src/app/js/libs/jsgrader.js
+++ src/app/js/libs/jsgrader.js
@@ -1,5 +1,6 @@
 /**
- * @fileOverview This file contains the JSGrader library to test the JavaScript context.
+ * @fileOverview This file contains the JSGrader library to test the JavaScript
+ * context.
  * @name jsgrader.js<libs>
  * @author Cameron Pittman
  * @license GPLv3
@@ -13,22 +14,21 @@ var Grader = (function() {
     function compare2Objects (x, y) {
       var p;
 
-      // remember that NaN === NaN returns false
-      // and isNaN(undefined) returns true
+      // remember that NaN === NaN returns false and isNaN(undefined) returns
+      // true
       if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
         return true;
       }
 
-      // Compare primitives and functions.
-      // Check if both arguments link to the same object.
-      // Especially useful on step when comparing prototypes
+      // Compare primitives and functions.  Check if both arguments link to the
+      // same object.  Especially useful on step when comparing prototypes
       if (x === y) {
         return true;
       }
 
-      // Works in case when functions are created in constructor.
-      // Comparing dates is a common scenario. Another built-ins?
-      // We can even handle functions passed across iframes
+      // Works in case when functions are created in constructor.  Comparing
+      // dates is a common scenario. Another built-ins?  We can even handle
+      // functions passed across iframes
       if ((typeof x === 'function' && typeof y === 'function') ||
         (x instanceof Date && y instanceof Date) ||
         (x instanceof RegExp && y instanceof RegExp) ||
@@ -104,7 +104,9 @@ var Grader = (function() {
     }
 
     if (arguments.length < 1) {
-      return true; //Die silently? Don’t know how to handle such case, please help...
+      // Die silently? Don’t know how to handle such case, please
+      // help...
+      return true;
       // throw "Need two or more arguments to compare";
     }
 
@@ -398,7 +400,8 @@ var Grader = (function() {
     },
 
     isjQuery: function (elem) {
-      // could use obj.jquery, which will only return true if it is a jquery object
+      // could use obj.jquery, which will only return true if it is a jquery
+      // object
       var isjQ = false;
       if (elem instanceof $) {
         isjQ = true;
